07a0b39005aa181c3bc5617d0f8147cd636902ed
agrinceac <sashagrinceac1>
2018-01-12 16:33:38 +0200

    Реализовал и вывел дополнительную категорию Новинки на Лероме

--- core/modules/categories/CategoriesAliasesRules.php
+++ core/modules/categories/CategoriesAliasesRules.php
@@ -4,7 +4,8 @@ class CategoriesAliasesRules
 {
 	private $rules = array(
 		'/dana/news/' => '/news/',
-        '/ug/news_ug/'   => '/news_ug/'
+        '/ug/news_ug/'   => '/news_ug/',
+        '/akcii/new-lerom-products/' => '/new-lerom-products/'
 	);
 
 	public function useRules($alias)
